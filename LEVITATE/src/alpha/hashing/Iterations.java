ad81d475bbdf89f9d31d149451f89b4a4e924135
Nishant Gaurav <ng19nishant@gmail.com>
2023-08-22 23:53:13 +0530

    Iterate in HashMap

---
+++ LEVITATE/src/alpha/hashing/Iterations.java
@@ -0,0 +1,16 @@
+package alpha.hashing;
+import java.util.*;
+public class Iterations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String , Integer> map= new HashMap<>();
+		
+		map.put("India", 100);
+		map.put("China", 150);
+		map.put("USA", 70);
+		map.put("Africa", 50);
+
+	}
+
+}

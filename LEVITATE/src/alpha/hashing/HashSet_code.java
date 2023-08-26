e7522c6acc3952194da1ab84c4b9ba02f098800f
Nishant Gaurav <ng19nishant@gmail.com>
2023-08-26 17:14:22 +0530

    HashSet and its methods.

---
+++ LEVITATE/src/alpha/hashing/HashSet_code.java
@@ -0,0 +1,31 @@
+package alpha.hashing;
+import java.util.*;
+
+public class HashSet_code {
+
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+        System.out.println(set);// only unique element will get printed.
+        //set.remove(2);
+        
+        if (set.contains(2)) {
+        	System.out.println("set contains 2");
+        }
+        if(set.contains(3)) {
+        	System.out.println("set contains 3");
+        }
+        
+        //System.out.println(set);
+        
+        System.out.println(set.size());
+        set.clear();
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+    }
+}
